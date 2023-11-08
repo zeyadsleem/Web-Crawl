e2575431e09108c2d4fc86f435bdb3abfb408c0d
zeyadsleem <zeyadsleem1@gmail.com>
2023-11-08 02:03:34 +0200

    add new functions and tests to crawl

--- crawl.spec.mjs
+++ crawl.spec.mjs
@@ -1,30 +1,63 @@
-import { expect, test } from 'vitest';
-import { normalizeURL } from "./crawl.mjs";
+import { normalizeURL, getURLsFromHTML } from './crawl.mjs'
+import { test, expect } from 'vitest'
 
-test('normalizeURL strip protocol', () => {
-  const input = 'https://mail.google.com/mail';
-  const actual = normalizeURL(input);
-  const expected = 'mail.google.com/mail';
-  expect(actual).toEqual(expected);
+test('normalizeURL protocol', () => {
+  const input = 'https://mail.google.com/path'
+  const actual = normalizeURL(input)
+  const expected = 'mail.google.com/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strip slash', () => {
-  const input = 'https://mail.google.com/mail/';
-  const actual = normalizeURL(input);
-  const expected = 'mail.google.com/mail';
-  expect(actual).toEqual(expected);
+test('normalizeURL slash', () => {
+  const input = 'https://mail.google.com/path/'
+  const actual = normalizeURL(input)
+  const expected = 'mail.google.com/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL Capitals', () => {
-  const input = 'https://mail.Google.com/mail/';
-  const actual = normalizeURL(input);
-  const expected = 'mail.google.com/mail';
-  expect(actual).toEqual(expected);
+test('normalizeURL capitals', () => {
+  const input = 'https://mail.google.com/path'
+  const actual = normalizeURL(input)
+  const expected = 'mail.google.com/path'
+  expect(actual).toEqual(expected)
 })
 
-test('normalizeURL strpi http', () => {
-  const input = 'https://mail.Google.com/mail/';
-  const actual = normalizeURL(input);
-  const expected = 'mail.google.com/mail';
-  expect(actual).toEqual(expected);
+test('normalizeURL http', () => {
+  const input = 'http://mail.google.com/path'
+  const actual = normalizeURL(input)
+  const expected = 'mail.google.com/path'
+  expect(actual).toEqual(expected)
 })
+
+test('getURLsFromHTML absolute', () => {
+  const inputURL = 'https://mail.google.com'
+  const inputBody = '<html><body><a href="https://mail.google.com"><span>google.com></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://mail.google.com/']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+  const inputURL = 'https://mail.google.com'
+  const inputBody = '<html><body><a href="/path/one"><span>google.com></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://mail.google.com/path/one']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+  const inputURL = 'https://mail.google.com'
+  const inputBody = '<html><body><a href="/path/one"><span>google.com></span></a><a href="https://other.com/path/one"><span>google.com></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = ['https://mail.google.com/path/one', 'https://other.com/path/one']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML handle error', () => {
+  const inputURL = 'https://mail.google.com'
+  const inputBody = '<html><body><a href="path/one"><span>google.com></span></a></body></html>'
+  const actual = getURLsFromHTML(inputBody, inputURL)
+  const expected = []
+  expect(actual).toEqual(expected)
+})
+
